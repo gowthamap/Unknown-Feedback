bb58059c2abf7ed8e3bd1046c316ff2c84475db8
Gowthamap <gowtham.p@vonnue.com>
2025-09-21 14:07:15 +0530

    feat: multiSelector component

---
+++ app/components/multiSelector.tsx
@@ -0,0 +1,105 @@
+"use client";
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+// import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+const people = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Mary" },
+  { id: 3, name: "Alex" },
+  { id: 4, name: "Sara" },
+  { id: 6, name: "David" },
+  { id: 7, name: "Bilsy" },
+  { id: 8, name: "Soorya" },
+  { id: 9, name: "Chinju" },
+  { id: 10, name: "Riza Rahmath" },
+  { id: 11, name: "Gouri Nandana" },
+  { id: 12, name: "Shamil" },
+];
+
+export default function MultiSelectWithSearch() {
+  const [selectedPeople, setSelectedPeople] = useState<typeof people>([]);
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) =>
+          person.name.toLowerCase().includes(query.toLowerCase())
+        );
+
+  return (
+    <div className="w-full">
+      <h2 className="mb-2 font-medium">Feedback recipient(s)</h2>
+      <Combobox value={selectedPeople} onChange={setSelectedPeople} multiple>
+        <div className="relative">
+          <div className="relative w-full cursor-default overflow-hidden rounded-xl border border-blue-300 bg-white text-left shadow-sm focus-within:ring-2 focus-within:ring-blue-500 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-3 pl-3 pr-10 leading-5 text-gray-900 focus:ring-0 outline-none"
+              displayValue={(people: typeof selectedPeople) =>
+                people.map((p) => p.name).join(", ")
+              }
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Search and select..."
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              {/* <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              /> */}
+            </Combobox.Button>
+          </div>
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-xl bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-10">
+              {filteredPeople.length === 0 && query !== "" ? (
+                <div className="relative cursor-default select-none px-4 py-2 text-gray-500">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredPeople.map((person) => (
+                  <Combobox.Option
+                    key={person.id}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? "bg-blue-100 text-blue-900" : "text-gray-900"
+                      }`
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {person.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-blue-600" : "text-blue-500"
+                            }`}
+                          >
+                            {/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+}
